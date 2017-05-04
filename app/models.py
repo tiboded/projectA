a37ebf8f4b5354ae0fc578fda060191d1931381d
tiboded <tiboded@hotmail.fr>
2017-05-04 21:36:55 +0800

    NA

--- app/models.py
+++ app/models.py
@@ -3,16 +3,16 @@ from flask_login import UserMixin
 
 class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
-    email=db.Column(db.String)
-    username=db.Column(db.String)
-    password=db.Column(db.String)
+    email=db.Column(db.String(24))
+    username=db.Column(db.String(24))
+    password=db.Column(db.String(96))
     admin=db.Column(db.Boolean)
     project_id=db.Column(db.Integer)
     # project=db.relationship('Project',backref='user',lazy='dynamic')
 
 class Project(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String)
-    text=db.Column(db.String)
+    title=db.Column(db.String(48))
+    text=db.Column(db.String(1024))
     user_id=db.Column(db.Integer)
     # user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
